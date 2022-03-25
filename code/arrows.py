7003ff27cc3aa312bd0d0f109b09e613722bbc2f
qitianshi <65122416+qitianshi@users.noreply.github.com>
2022-03-25 15:04:48 +0800

    Add rotate_tail method

--- code/arrows.py
+++ code/arrows.py
@@ -16,3 +16,15 @@ def rotate_center(xcenter, ycenter, length, rotation) -> tuple[float]:
         length * np.sin(rad_angle),
         length * np.cos(rad_angle)
     )
+
+def rotate_tail(xtail, ytail, length, rotation) -> tuple[float]:
+    """Rotates an arrow of a specified length at its tail."""
+
+    rad_angle = np.radians(rotation)
+
+    return (
+        xtail,
+        ytail,
+        length * np.sin(rad_angle),
+        length * np.cos(rad_angle)
+    )
